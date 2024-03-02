47dccd0cee4aa3dc0c68e42dd69da7c51e6fb365
Geuni <dlrmsgnl620@naver.com>
2024-03-02 16:07:12 +0900

    test: react 18버전 automatich batching

--- react18/src/App.tsx
+++ react18/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+let rerender = 0;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  //   setFlag(!flag);
+  // };
+
+  const handleClick = () => {
+    setTimeout(() => {
+      setCount(count + 1);
+      setFlag(!flag);
+    }, 0);
+  };
+
+  useEffect(() => {
+    rerender++;
+    console.log(
+      "몇 번의 렌더링이 이루어지나요?",
+      rerender,
+      "counter: ",
+      count,
+      "flag: ",
+      flag
+    );
+  }, [count, flag]);
 
   return (
-    <>
+    <section>
+      <h1>React 18 + Vite</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Count: {count}</p>
+        <p>Flag: {flag.toString()}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <img src={reactLogo} alt="React Logo" width="120" />
+      <img src={viteLogo} alt="Vite Logo" width="120" />
+      <p>
+        <button onClick={handleClick}>count is: {count}</button>
       </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
